297bcf4f686a844bdc78167274b6d29b4d263239
Lai <jingchyuan00@gmail.com>
2026-01-05 10:47:12 +0800

    FC_SCRIPT

---
+++ bash_main.py
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e   # stop if any command fails
+
+python3 /home/inf25/work/fc_script/done_local_script/script_python
+
+python3 /home/inf25/work/fc_script/done_local_script/script_python2
+
+python3 /home/inf25/work/fc_script/done_local_script/script_python3
+
+rm -rf fc_command.log
+rm -rf fc_output.txt
