74fe126220db5be5603717f14253e1af9fb70276
Lai <jingchyuan00@gmail.com>
2026-01-05 13:38:51 +0800

    FC_SCRIPT

--- bash_main.py
+++ bash_main.py
@@ -2,11 +2,12 @@
 
 set -e   # stop if any command fails
 
-python3 /home/inf25/work/fc_script/done_local_script/script_python
+python3 /home/inf25/work/fc_script/test_local_script/script_python
 
-python3 /home/inf25/work/fc_script/done_local_script/script_python2
+python3 /home/inf25/work/fc_script/test_local_script/script_python2
 
-python3 /home/inf25/work/fc_script/done_local_script/script_python3
+python3 /home/inf25/work/fc_script/test_local_script/script_python3
 
 rm -rf fc_command.log
 rm -rf fc_output.txt
+rm -rf logs_report/*.bak
